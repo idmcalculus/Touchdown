1738952e45b96d18443563eeada25c594a071087
Damilola Michael Ige <idm.calculus@gmail.com>
2022-02-10 08:03:44 +0100

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by IGE DAMILOLA MICHAEL on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
